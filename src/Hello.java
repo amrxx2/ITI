1c022c46b94953cdb3bc3dde8f4c592a7f899b1c
xAmrxxx <amrmoneer21@gmail.com>
2021-04-19 13:10:15 +0200

    commiting my only java file.

---
+++ src/Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+    public static void main (String [] args){
+        System.out.println("helllooooooowwww");
+        for(int i = 0; i < 5; i++){
+
+            System.out.println("helllooooooowwww");
+
+        }
+    }
+}
